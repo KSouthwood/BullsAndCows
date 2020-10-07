16b1ad303c9a5c715accaa3c67510873719b2573
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-10-07 17:03:35 -0400

    [feat] complete Stage 3
    - rename getInput() to getGuess() and add getCodeLength() method. Needed to do because the parameters for the input are different
    - add secret code generation routine

--- bullscows/Controller.java
+++ bullscows/Controller.java
@@ -7,8 +7,10 @@ public class Controller {
 
     public void controller() {
         while (true) {
-            String guess = getInput();
-            grade("1357", guess);
+            int length = getCodeLength();
+            System.out.println("The random secret number is " + generateSecretCode(length));
+//            String guess = getGuess();
+//            grade("1357", guess);
             return;
         }
     }
@@ -38,7 +40,7 @@ public class Controller {
         System.out.printf(" The secret code is %s.%n", code);
     }
 
-    private String getInput() {
+    private String getGuess() {
         String input;
         boolean goodInput = false;
 
@@ -52,5 +54,57 @@ public class Controller {
         } while (!goodInput);
 
         return input;
+    }
+
+    private int getCodeLength() {
+        String input;
+
+        do {
+            input = scanner.nextLine();
+            if (!input.matches("\\d+")) {
+                System.out.println("Please enter a number between 1 and 9.");
+            }
+        } while (!input.matches("\\d+"));
+
+        return Integer.parseInt(input);
+    }
+
+    private String generateSecretCode(int codeLength) {
+        if (codeLength < 1 || codeLength > 10) {
+            System.out.println("Error: length of code must be between 1 and 10.");
+            return null;
+        }
+
+        StringBuilder secretCode;
+
+        do {
+            secretCode = new StringBuilder(codeLength);
+            long pseudoRandomNumber = System.nanoTime();
+            boolean[] digitUsed = new boolean[10];
+            int length = codeLength;
+
+            if (codeLength == 1) {
+                digitUsed[0] = true;
+            }
+
+            while (pseudoRandomNumber > 0 && length > 0) {
+                int digit = (int) (pseudoRandomNumber % 10);
+                pseudoRandomNumber /= 10;
+                if (!digitUsed[digit]) {
+                    digitUsed[digit] = true;
+                    secretCode.append(digit);
+                    length--;
+                }
+            }
+        } while (secretCode.length() != codeLength);
+
+        if (secretCode.charAt(0) == '0') {
+            int swapWith = (Integer.parseInt(secretCode.substring(1, 2)) % (codeLength - 1)) + 1;
+            char swapDigit = secretCode.charAt(swapWith);
+            secretCode.deleteCharAt(0).deleteCharAt(swapWith);
+            secretCode.insert(0, swapDigit).insert(swapWith, '0');
+        }
+
+        return String.valueOf(secretCode);
     }
 }
